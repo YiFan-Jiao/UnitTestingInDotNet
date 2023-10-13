8a84873dc6e870681b5425f74a73c8c7f45b6be3
YiFan <jiao1995cn@gmail.com>
2023-10-12 21:42:36 -0500

    添加项目文件。

---
+++ UnitTestingInDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnitTestingInDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
